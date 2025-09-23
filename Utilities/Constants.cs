a49404fb71c82167f67d8e5215e912ea67ab83c6
Min Naing Tun <minnaingtun.pro@gmail.com>
2025-09-23 08:32:10 +0630

    Update mapping table.

--- Utilities/Constants.cs
+++ Utilities/Constants.cs
@@ -5,6 +5,9 @@ namespace DER_System.Utilities
     public class Constants
     {
         //Mappings Table
+        public string MaterialType = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build()["TableMapping:MaterialType"]!;
+        public string Route = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build()["TableMapping:Route"]!;
+        public string User = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build()["TableMapping:User"]!;
         public string Customer = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build()["TableMapping:Customer"]!;
         public string Material = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build()["TableMapping:Material"]!;
         public string CustomerMaterialListing = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build()["TableMapping:CustomerMaterialListing"]!;
