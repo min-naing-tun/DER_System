3883d328e14097be387866ff29bac3c0c2ec2393
Min Naing Tun <minnaingtun.pro@gmail.com>
2025-09-24 11:11:47 +0630

    Update api log model.

--- Model/API_Logs.cs
+++ Model/API_Logs.cs
@@ -7,7 +7,9 @@ namespace DER_System.Model
         [Key]
         public int Id { get; set; }
         public string Type { get; set; } //request or response
+        public string Method_Or_Status_Code { get; set; }
         public string Endpoint { get; set; }
+        public string Parameters { get; set; }
         public string Data { get; set; }
         public DateTime ActionTime { get; set; }
     }
